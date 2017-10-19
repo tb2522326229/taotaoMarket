1b3a273fa143cd7af6f27c75392144894dfb25aa
tb2522326229 <32827458+tb2522326229@users.noreply.github.com>
2017-10-19 18:18:23 +0800

    添加首页中各种首页的操纵

--- taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/PageContorller.java
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/PageContorller.java
@@ -1,6 +1,7 @@
 package com.taotao.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 //用于跳转到首页
@@ -10,5 +11,11 @@ public class PageContorller {
 	@RequestMapping("/")
 	public String showIndex(){
 		return "index";
+	}
+	
+	/*因为请求和路径和页面名称一样，所以这里直接一步到位，把路径提取出来后return给mvc.xml*/
+	@RequestMapping("/{page}")
+	public String showPage(@PathVariable String page){
+		return page;
 	}
 }
