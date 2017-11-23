9015261a94b0e10a2be0fc8579e9111e341e09e6
tb2522326229 <32827458+tb2522326229@users.noreply.github.com>
2017-11-23 12:05:54 +0800

    添加if语句是因为返回的url少了一个“/”，导致文件夹路径错误：例如： http://192.168.25.133/group1M00/00/00/wKgZhVoWSGiAItI4AALlmdmYimQ895.jpg 正确的是：http://192.168.25.133/group1/M00/00/00/wKgZhVoWSGiAItI4AALlmdmYimQ895.jpg

--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/PictureServiceImpl.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/PictureServiceImpl.java
@@ -32,17 +32,26 @@ public class PictureServiceImpl implements PictureService {
 		String extName = filename.substring(filename.lastIndexOf(".") + 1);
 		// 上传到图片服务器
 		try {
-			FastDFSClient fastdfs = new FastDFSClient("classpath:properties/Client.conf");
+			FastDFSClient fastdfs = new FastDFSClient(
+					"E:/JAVA/GitHub/taotaoMarket/taotao-manager/taotao-manager-web/src/main/resources/properties/Client.conf");
 			String[] urls = fastdfs.uploadFile(picFile.getBytes(), extName);
-			System.out.println("urls: " + urls);
 			String url = IMAGE_BASE_URL;
 			// String url = "192.168.133.25";
 			for (int i = 0; i < urls.length; i++) {
-				url += urls[i];
+				/*
+					添加if语句是因为返回的url少了一个“/”，导致文件夹路径错误：例如：
+					http://192.168.25.133/group1M00/00/00/wKgZhVoWSGiAItI4AALlmdmYimQ895.jpg
+					正确的是：http://192.168.25.133/group1/M00/00/00/wKgZhVoWSGiAItI4AALlmdmYimQ895.jpg
+				 */
+				if(i == 0){
+					url += urls[i] + "/";
+				}
+				else{
+					url += urls[i];
+				}
 			}
 			System.out.println("url: " + url);
 			result.setError(0);
-			result.setUrl(url);
 
 		} catch (Exception e) {
 			e.printStackTrace();
