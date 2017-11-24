55da876aaf4624531acc7ec34faa1aa827bced33
tb2522326229 <32827458+tb2522326229@users.noreply.github.com>
2017-11-24 15:52:21 +0800

    因为是post方法提交表单，所有告诉了代码中页面的提交方法

--- taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/TbItemController.java
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/TbItemController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.taotao.common.entity.EasyUIDataGridResult;
@@ -30,7 +31,7 @@ public class TbItemController {
 		return itemList;
 	}
 	
-	@RequestMapping("/item/save")
+	@RequestMapping(value = "/item/save",method = RequestMethod.POST)
 	@ResponseBody
 	public TaotaoResult createItem(TbItem item,String desc){
 		TaotaoResult result = itemService.createItem(item, desc);
