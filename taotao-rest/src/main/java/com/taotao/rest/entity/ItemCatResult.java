8ccf33a5447fbce2fa9ec9cb3b1bf2c6a94cf966
tb2522326229 <32827458+tb2522326229@users.noreply.github.com>
2017-12-02 19:01:34 +0800

    创建一个类，用来描述根节点“data”。json格式中，key是data，value才是CatagoryNode

---
+++ taotao-rest/src/main/java/com/taotao/rest/entity/ItemCatResult.java
@@ -0,0 +1,16 @@
+package com.taotao.rest.entity;
+
+import java.util.List;
+
+public class ItemCatResult {
+	private List data;
+
+	public List getData() {
+		return data;
+	}
+
+	public void setData(List data) {
+		this.data = data;
+	}
+
+}
