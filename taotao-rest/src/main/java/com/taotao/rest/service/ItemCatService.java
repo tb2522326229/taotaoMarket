37dde639b80966264785a092799f9bc2c55c4ef3
tb2522326229 <32827458+tb2522326229@users.noreply.github.com>
2017-12-03 22:37:14 +0800

    从数据库中获取真正的商品分类列表并封装成json格式发送给服务器，其中dao:从tb_item_cat表中取数据，单表查询，可以使用逆向工程生成的代码;service:根据parentid查询子节点列表，并递归调用;controller:响应一个json数据。判断callback参数是否为空，如果为空正常返回json数据，如果不为空，支持jsonp调用。lib-v1:修改url 来是portal可以访问rest服务

---
+++ taotao-rest/src/main/java/com/taotao/rest/service/ItemCatService.java
@@ -0,0 +1,8 @@
+package com.taotao.rest.service;
+
+import com.taotao.rest.entity.ItemCatResult;
+
+public interface ItemCatService {
+	// 
+	public ItemCatResult getItemCatList();
+}
---
+++ taotao-rest/src/main/java/com/taotao/rest/service/impl/ItemCatServiceImpl.java
@@ -0,0 +1,65 @@
+package com.taotao.rest.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.taotao.entity.TbItemCat;
+import com.taotao.entity.TbItemCatExample;
+import com.taotao.entity.TbItemCatExample.Criteria;
+import com.taotao.entity.TbItemExample;
+import com.taotao.mapper.TbItemCatMapper;
+import com.taotao.rest.entity.CatgoryNode;
+import com.taotao.rest.entity.ItemCatResult;
+import com.taotao.rest.service.ItemCatService;
+
+@Service
+public class ItemCatServiceImpl implements ItemCatService {
+	@Autowired
+	private TbItemCatMapper itemCatMapper;
+
+	// 查询商品种类
+	@Override
+	public ItemCatResult getItemCatList() {
+		// 调用递归方法来查询商品分类列表
+		List catList = getItemCatList(0L);
+		//返回结果
+		ItemCatResult result = new ItemCatResult();
+		result.setData(catList);
+		return result;
+	}
+
+	// 递归方法通过parent_id查询商品分类
+	public List getItemCatList(Long parentId) {
+		// 根据parentId查询列表
+		TbItemCatExample example = new TbItemCatExample();
+		Criteria criteria = example.createCriteria();
+		criteria.andParentIdEqualTo(parentId);
+		// 执行查询
+		List<TbItemCat> list = itemCatMapper.selectByExample(example);
+		List resultList = new ArrayList<>();
+		for (TbItemCat tbItemCat : list) {
+			// 如果是父节点
+			if (tbItemCat.getIsParent()) {
+				CatgoryNode node = new CatgoryNode();
+				node.setUrl("/products/" + tbItemCat.getId() + ".html");
+				// 如果当前节点为第一级节点
+				if (tbItemCat.getParentId() == 0) {
+					node.setName("<a href='/products/" + tbItemCat.getId() + ".html'>" + tbItemCat.getName() + "</a>");
+				} else {
+					node.setName(tbItemCat.getName());
+				}
+				node.setItems(getItemCatList(tbItemCat.getId()));
+				// 把node添加到列表
+				resultList.add(node);
+			} else {
+				// 如果是叶子节点
+				String item = "/products/" + tbItemCat.getId() + ".html|" + tbItemCat.getName();
+				resultList.add(item);
+			}
+		}
+		return resultList;
+	}
+}
