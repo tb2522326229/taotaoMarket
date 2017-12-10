d8319ac73abb9ca9c8b103d97b4b9c06da0fd853
tb2522326229 <32827458+tb2522326229@users.noreply.github.com>
2017-12-10 17:59:19 +0800

    去掉不需要的东西

--- taotao-common/src/main/java/com/taotao/common/utils/FastDFSClient.java
+++ taotao-common/src/main/java/com/taotao/common/utils/FastDFSClient.java
@@ -3,7 +3,6 @@ package com.taotao.common.utils;
 import org.csource.common.NameValuePair;
 import org.csource.fastdfs.ClientGlobal;
 import org.csource.fastdfs.StorageClient;
-import org.csource.fastdfs.StorageClient1;
 import org.csource.fastdfs.StorageServer;
 import org.csource.fastdfs.TrackerClient;
 import org.csource.fastdfs.TrackerServer;
