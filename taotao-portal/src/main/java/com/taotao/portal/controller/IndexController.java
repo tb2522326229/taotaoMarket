74b24f098922d1fd7bc15cb1145b632fb6c20863
tb2522326229 <32827458+tb2522326229@users.noreply.github.com>
2017-12-11 15:18:29 +0800

    设置轮播图

--- taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
+++ taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
@@ -1,13 +1,24 @@
 package com.taotao.portal.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.taotao.common.utils.JsonUtils;
+import com.taotao.portal.service.ContentService;
+
 @Controller
 public class IndexController {
+	
+	@Autowired
+	private ContentService contentService;
+	
 	@RequestMapping("/index")
-	public String showIndex() {
+	public String showIndex(Model model) {
+		String list = contentService.getAd1List();
+		model.addAttribute("ad1",list);
 		return "index";
 	}
 	
---
+++ taotao-portal/src/main/java/com/taotao/portal/service/ContentService.java
@@ -0,0 +1,7 @@
+package com.taotao.portal.service;
+
+public interface ContentService {
+
+	// 获取达广告位的轮播图
+	public String getAd1List();
+}
