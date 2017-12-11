dbbac8b33fde6d88b65ff773f33b43e679ea3f35
tb2522326229 <32827458+tb2522326229@users.noreply.github.com>
2017-12-11 12:45:38 +0800

    将httpclient提取出一个工具类并测试

--- taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
+++ taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
@@ -2,12 +2,20 @@ package com.taotao.portal.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
 	@RequestMapping("/index")
 	public String showIndex() {
 		return "index";
+	}
+	
+	@RequestMapping(value="/posttest",method=RequestMethod.POST)
+	public String postTest(String name , String pass) {
+		System.out.println(name);
+		System.out.println(pass);
+		return "OK";
 	}
 
 }
