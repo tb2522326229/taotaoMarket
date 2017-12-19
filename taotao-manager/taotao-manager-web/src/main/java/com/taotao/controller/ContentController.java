06a28c712933bbdfa3e413d291cf25f0f384fddd
tb2522326229 <32827458+tb2522326229@users.noreply.github.com>
2017-12-19 12:36:11 +0800

    后台（增删改）某一个cid对应的内容时，应该同步缓存（将原来未被修改的内容删除后，添加修改后的内容）

--- taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ContentController.java
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ContentController.java
@@ -1,12 +1,14 @@
 package com.taotao.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.taotao.common.entity.EasyUIDataGridResult;
+import com.taotao.common.utils.HttpClientUtil;
 import com.taotao.common.utils.TaotaoResult;
 import com.taotao.entity.TbContent;
 import com.taotao.service.ContentService;
@@ -14,6 +16,10 @@ import com.taotao.service.ContentService;
 @Controller
 @RequestMapping("/content")
 public class ContentController {
+	@Value("${REST_BASE_URL}")
+	private String REST_BASE_URL;
+	@Value("${REST_CONTENT_SYNC_URL}")
+	private String REST_CONTENT_SYNC_URL;
 	@Autowired
 	private ContentService contentService;
 	
@@ -36,6 +42,8 @@ public class ContentController {
 	@ResponseBody
 	public TaotaoResult insertContent(TbContent content) {
 		TaotaoResult result = contentService.insertContent(content);
+		// 调用taotao-rest发布的服务，同步缓存
+		HttpClientUtil.doGet(REST_BASE_URL + REST_CONTENT_SYNC_URL + content.getCategoryId());
 		return result;
 	}
 
