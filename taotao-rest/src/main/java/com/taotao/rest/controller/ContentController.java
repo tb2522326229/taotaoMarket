ed58d7d3f36dc17e63decfb64eb43713f0840f36
tb2522326229 <32827458+tb2522326229@users.noreply.github.com>
2017-12-19 12:15:11 +0800

    为了减轻数据库的压力，给每个内容都添加了缓存。用户在登陆商城首页并查询某个cid对应的内容后，第一次查询会根据cid查询数据库，并将查询到的内容添加到缓存（hset(方法)），下次就可以直接根据查询缓存而不必查询数据库。如果某个cid下的内容进行了修改，则原来缓存中的内容会直接删除，并重新查询数据库后添加会redis（类似于脏检查）

--- taotao-rest/src/main/java/com/taotao/rest/controller/ContentController.java
+++ taotao-rest/src/main/java/com/taotao/rest/controller/ContentController.java
@@ -17,13 +17,26 @@ import com.taotao.rest.service.ContentService;
 public class ContentController {
 	@Autowired
 	private ContentService contentService;
-	
+
 	@RequestMapping("/content/{cid}")
 	@ResponseBody
 	public TaotaoResult getContentList(@PathVariable Long cid) {
 		try {
 			List<TbContent> list = contentService.getContentList(cid);
 			return TaotaoResult.ok(list);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return TaotaoResult.build(500, ExceptionUtil.getStackTrace(e));
+		}
+	}
+	
+	// 后台修改了cid对应的内容后应该进行的操作
+	@RequestMapping("/sync/content/{cid}")
+	@ResponseBody
+	public TaotaoResult sysncContent(@PathVariable Long cid) {
+		try {
+			TaotaoResult result = contentService.syncContent(cid);
+			return result;
 		} catch (Exception e) {
 			e.printStackTrace();
 			return TaotaoResult.build(500, ExceptionUtil.getStackTrace(e));
--- taotao-rest/src/main/java/com/taotao/rest/service/ContentService.java
+++ taotao-rest/src/main/java/com/taotao/rest/service/ContentService.java
@@ -2,10 +2,12 @@ package com.taotao.rest.service;
 
 import java.util.List;
 
+import com.taotao.common.utils.TaotaoResult;
 import com.taotao.entity.TbContent;
 
 public interface ContentService {
 	// 发布内容查询服务：根据内容分类id查询tb_content表，得到此分类下的内容列表，返回。
 	public List<TbContent> getContentList(Long cid);
-
+	// 缓存同步：当后台修改内容信息后，只需要把redis中缓存的数据删除即可。
+	public TaotaoResult syncContent(Long cid);
 }
