f7ef0aa98165731e621d97772fd7651a53604230
tb2522326229 <32827458+tb2522326229@users.noreply.github.com>
2017-12-27 19:53:11 +0800

    取商品列表，遍历列表，创建文档对象，把文档对象写入索引库。

---
+++ taotao-search/src/main/java/com/taotao/search/mapper/ItemMapper.java
@@ -0,0 +1,9 @@
+package com.taotao.search.mapper;
+
+import java.util.List;
+
+import com.taotao.search.entity.SearchItem;
+
+public interface ItemMapper {
+	List<SearchItem> getItemList();
+}
---
+++ taotao-search/src/main/java/com/taotao/search/service/ItemService.java
@@ -0,0 +1,8 @@
+package com.taotao.search.service;
+
+import com.taotao.common.utils.TaotaoResult;
+
+public interface ItemService {
+	// 导入商品信息
+	public TaotaoResult importItems() throws Exception;
+}
